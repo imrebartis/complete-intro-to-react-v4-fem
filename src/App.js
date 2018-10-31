766ae499be9b99cda1168bd50463bf03854da685
Imre Bartis <imre.bartis@siili.com>
2018-10-31 18:27:16 +0200

    pass component data using props

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends React.Component {
             } else {
               breed = pet.breeds.breed
             }
-            return <Pet key={pet.id} animal={pet.animal} name={pet.name} breed={breed} />
+            return <Pet key={pet.id} animal={pet.animal} name={pet.name} breed={breed} media={pet.media} location={`${pet.contact.city}, ${pet.contact.state}`} />
         })}
       </div>
     );
