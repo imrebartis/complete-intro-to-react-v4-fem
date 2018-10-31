b90fbb0de6a0a2468b32e76f539bb4abe1dc39a4
Imre Bartis <imre.bartis@siili.com>
2018-10-31 17:50:43 +0200

    debug component state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import pf from "petfinder-client";
-import Pet from "./Pet";
+// import Pet from "./Pet";
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -10,17 +10,42 @@ const petfinder = pf({
 })
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pets: []
+    }
+  }
   componentDidMount() {
-    const promise = petfinder.breed.list({animal: "dog"})
-    promise.then(console.log, console.error)
+    petfinder.pet.find({output: "full", location: "Seattle, WA"})
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet
+          } else {
+            pets = [data.petfinder.pets.pet]
+          }
+        } else {
+          pets = []
+        }
+
+        this.setState({
+          pets
+        })
+
+      })
   }
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        <Pet name="Luna" animal="dog" breed="Havanese" />
-        <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-        <Pet name="Doink" animal="cat" breed="Mix" />
+        <pre>
+        // dump all the state to the page, i.e. see all the data from the API
+          <code>{JSON.stringify(this.state, null, 4)}</code>
+        </pre>
       </div>
     );
   }
