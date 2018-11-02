dd0e728eacaa471f2ece813410988053737e50fd
Imre Bartis <imre.bartis@siili.com>
2018-11-02 07:39:36 +0200

    refactor components for adding routing

--- src/App.js
+++ src/App.js
@@ -1,60 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import pf from "petfinder-client";
-import Pet from "./Pet";
-
-const petfinder = pf({
-  key: process.env.API_KEY,
-  secret: process.env.API_SECRET
-
-})
+import Results from "./Results";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      pets: []
-    }
-  }
-  componentDidMount() {
-    petfinder.pet.find({output: "full", location: "Seattle, WA"})
-      .then(data => {
-        let pets;
-
-        if (data.petfinder.pets && data.petfinder.pets.pet) {
-          if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet
-          } else {
-            pets = [data.petfinder.pets.pet]
-          }
-        } else {
-          pets = []
-        }
-
-        this.setState({
-          pets
-        })
-
-      })
-  }
   render() {
     return (
       <div>
         <h1>Adopt Me!</h1>
-        {this.state.pets.map(pet => {
-            let breed;
-
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(', ')
-            } else {
-              breed = pet.breeds.breed
-            }
-            return <Pet key={pet.id} animal={pet.animal} name={pet.name} breed={breed} media={pet.media} location={`${pet.contact.city}, ${pet.contact.state}`} />
-        })}
+        <Results/>
       </div>
-    );
+    )
   }
 }
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
