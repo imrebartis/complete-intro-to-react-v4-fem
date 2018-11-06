61a8e95c021937117237f8322b80fa42e5e25a46
Imre Bartis <imre.bartis@siili.com>
2018-11-06 17:54:25 +0200

    add carousel interactivity

--- src/Carousel.js
+++ src/Carousel.js
@@ -14,6 +14,13 @@ class Carousel extends React.Component {
   }
   return {photos}
  }
+
+ handleIndexClick = event => {
+   this.setState({
+     // turn string into a number with the plus sign
+    active: +event.target.dataset.index
+   })
+ }
  render() {
    const {photos, active} = this.state;
 
@@ -22,7 +29,15 @@ class Carousel extends React.Component {
       <img src={photos[active].value} alt="primary animal" />
       <div className="carousel-smaller">
         {photos.map((photo, index) => (
-          <img key={photo.value} src={photo.value} className={index === active ? 'active' : ''} alt="animal thumbnail"/>
+          /* eslint-disable-next-line */
+          <img
+            onClick={this.handleIndexClick}
+            key={photo.value}
+            data-index={index}
+            src={photo.value}
+            className={index === active ? 'active' : ''}
+            alt="animal thumbnail"
+          />
         ))}
       </div>
      </div>
