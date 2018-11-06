0acb76bc37a1b4d0ebe625c68f6d483e3cb1d609
Imre Bartis <imre.bartis@siili.com>
2018-11-06 18:19:19 +0200

    add a search input component & handle input state change

---
+++ src/SearchParams.js
@@ -0,0 +1,32 @@
+import React from 'React';
+
+class SearchParams extends React.Component {
+  state = {
+    location: "Seattle, WA",
+    animal: "",
+    breed: ""
+  };
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="search-params">
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={this.handleLocationChange}
+            id="location"
+            value={this.state.location}
+            placeholder="location"
+          />
+        </label>
+      </div>
+    )
+  }
+}
+
+export default SearchParams;
