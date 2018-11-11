a33f683d0cf1202b38925da16a30fffe1c4ea6bc
Imre Bartis <imre.bartis@siili.com>
2018-11-11 19:28:48 +0200

    use context in lifecycle methods

--- src/Results.js
+++ src/Results.js
@@ -1,60 +1,84 @@
 import React from "react";
 import pf from "petfinder-client";
+import { Consumer } from "./SearchContext";
 import Pet from "./Pet";
-import SearchBox from './SearchBox';
+import SearchBox from "./SearchBox";
 
 const petfinder = pf({
   key: process.env.API_KEY,
   secret: process.env.API_SECRET
-})
+});
 
 class Results extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       pets: []
-    }
+    };
   }
   componentDidMount() {
-    petfinder.pet.find({output: "full", location: "Seattle, WA"})
+    this.search();
+  }
+  search = () => {
+    petfinder.pet
+      .find({
+        output: "full",
+        location: this.props.searchParams.location,
+        animal: this.props.searchParams.animal,
+        breed: this.props.searchParams.breed
+      })
       .then(data => {
         let pets;
 
         if (data.petfinder.pets && data.petfinder.pets.pet) {
           if (Array.isArray(data.petfinder.pets.pet)) {
-            pets = data.petfinder.pets.pet
+            pets = data.petfinder.pets.pet;
           } else {
-            pets = [data.petfinder.pets.pet]
+            pets = [data.petfinder.pets.pet];
           }
         } else {
-          pets = []
+          pets = [];
         }
 
         this.setState({
           pets
-        })
-
-      })
+        });
+      });
   }
   render() {
-    throw new Error("hello error my old friend")
     return (
       <div className="search">
-        <SearchBox />
+        <SearchBox search={this.search}/>
         {this.state.pets.map(pet => {
-            let breed;
+          let breed;
 
-            if (Array.isArray(pet.breeds.breed)) {
-              breed = pet.breeds.breed.join(', ')
-            } else {
-              breed = pet.breeds.breed
-            }
-            return <Pet key={pet.id} animal={pet.animal} name={pet.name} breed={breed} media={pet.media} id={pet.id} location={`${pet.contact.city}, ${pet.contact.state}`} />
+          if (Array.isArray(pet.breeds.breed)) {
+            breed = pet.breeds.breed.join(", ");
+          } else {
+            breed = pet.breeds.breed;
+          }
+          return (
+            <Pet
+              key={pet.id}
+              animal={pet.animal}
+              name={pet.name}
+              breed={breed}
+              media={pet.media}
+              id={pet.id}
+              location={`${pet.contact.city}, ${pet.contact.state}`}
+            />
+          );
         })}
       </div>
     );
   }
 }
 
-export default Results;
+export default function ResultsWithContext(props) {
+  return (
+    <Consumer>
+      {context => <Results {...props} searchParams={context} />}
+    </Consumer>
+  );
+}
