a84dde24bac31022595cf90dba9cfd2a9bc3784d
Imre Bartis <imre.bartis@siili.com>
2018-11-20 07:05:28 +0200

    import emotion & extract a module

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import pf from "petfinder-client";
-import {Provider} from './SearchContext';
 import Results from "./Results";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import NavBar from "./NavBar";
+import { Provider } from "./SearchContext";
 
 const petfinder = pf({
   key: process.env.API_KEY,
   secret: process.env.API_SECRET
-})
+});
 
 class App extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      location: 'Seattle, WA',
-      animal: '',
-      breed: '',
+      location: "Seattle, WA",
+      animal: "",
+      breed: "",
       breeds: [],
       handleAnimalChange: this.handleAnimalChange,
       handleBreedChange: this.handleBreedChange,
       handleLocationChange: this.handleLocationChange,
       getBreeds: this.getBreeds
-
-    }
+    };
   }
-
   handleLocationChange = event => {
     this.setState({
       location: event.target.value
-    })
+    });
   };
   handleAnimalChange = event => {
     this.setState(
       {
-        animal: event.target.value,
-        breed: ""
+        animal: event.target.value
       },
       this.getBreeds
     );
@@ -46,12 +44,12 @@ class App extends React.Component {
   handleBreedChange = event => {
     this.setState({
       breed: event.target.value
-    })
+    });
   };
   getBreeds() {
     if (this.state.animal) {
       petfinder.breed
-        .list({animal: this.state.animal})
+        .list({ animal: this.state.animal })
         .then(data => {
           if (
             data.petfinder &&
@@ -60,9 +58,9 @@ class App extends React.Component {
           ) {
             this.setState({
               breeds: data.petfinder.breeds.breed
-            })
+            });
           } else {
-            this.setState({breeds: []});
+            this.setState({ breeds: [] });
           }
         })
         .catch(console.error);
@@ -72,29 +70,20 @@ class App extends React.Component {
       });
     }
   }
-
   render() {
     return (
       <div>
-        <header>
-        <Link to="/">Adopt Me!</Link>
-        <Link to="/search-params">
-          <span aria-label="search" role="img">
-            🔍
-          </span>
-        </Link>
-        </header>
+        <NavBar />
         <Provider value={this.state}>
           <Router>
-          <Results path="/"/>
-          <Details path="/details/:id" />
-          <SearchParams path="/search-params" />
+            <Results path="/" />
+            <Details path="/details/:id" />
+            <SearchParams path="/search-params" />
           </Router>
         </Provider>
       </div>
-    )
+    );
   }
 }
-
 
 ReactDOM.render(<App />, document.getElementById("root"));
