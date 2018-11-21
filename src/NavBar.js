1f00eef32fc66af21e62ae663ac9df8a87ef6dc2
Imre Bartis <imre.bartis@siili.com>
2018-11-21 06:59:13 +0200

    style components with emotion

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import {Link} from '@reach/router';
+import React from "react";
+import { Link } from "@reach/router";
+import styled from "react-emotion";
 
-const NavBar = () => {
-  <header>
+const Container = styled("header")`
+  position: sticky;
+  top: 0;
+  z-index: 10;
+`;
+
+const NavBar = () => (
+  <Container>
     <Link to="/">Adopt Me!</Link>
     <Link to="/search-params">
+      {/* eslint-disable-next-line */}
       <span aria-label="search" role="img">
         🔍
       </span>
     </Link>
-  </header>
-}
+  </Container>
+);
 
 export default NavBar;
