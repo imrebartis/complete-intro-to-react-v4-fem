30fa92693f3a7fe610da3c2553ccedfb3eda16b5
Imre Bartis <imre.bartis@siili.com>
2018-11-22 06:59:49 +0200

    reuse styles with js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,22 +1,35 @@
 import React from "react";
 import { Link } from "@reach/router";
 import styled from "react-emotion";
+import colors from './colors';
 
 const Container = styled("header")`
+  background-color: ${colors.dark};
   position: sticky;
   top: 0;
   z-index: 10;
 `;
 
+const NavLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+
+  span {
+    display: inline-block;
+    border: 1px solid red;
+  }
+`;
+
 const NavBar = () => (
   <Container>
-    <Link to="/">Adopt Me!</Link>
-    <Link to="/search-params">
+    <NavLink to="/">Adopt Me!</NavLink>
+    <NavLink to="/search-params">
       {/* eslint-disable-next-line */}
       <span aria-label="search" role="img">
         🔍
       </span>
-    </Link>
+    </NavLink>
   </Container>
 );
 
